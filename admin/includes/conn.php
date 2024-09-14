0e79d9c7373091630c5d00a0da01d6fdfb581b69
Abu Bakkar Siddique <144513910+absiddique-dev@users.noreply.github.com>
2024-09-14 06:33:27 +0530

    Update conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,17 +1,4 @@
 <?php
-// $host = 'localhost'; // Your database server hostname
-// $username = 'elavatet_elavate_arshad'; // Your database username
-// $password = 'Arshad@123#admin'; // Your database password
-// $database = 'elavatet_elavatetaxsolution'; // Your database name
-
-// $conn = mysqli_connect($host, $username, $password, $database);
-
-// // Check the connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-
 
 $host = 'localhost'; // Your database server hostname
 $username = 'root'; // Your database username
