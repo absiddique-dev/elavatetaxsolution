c5761cefa79bb4aca2fc240f927442ad15407071
Abu Bakkar Siddique <144513910+absiddique-dev@users.noreply.github.com>
2024-12-15 15:01:09 +0530

    Update conn.php

--- admin/conn.php
+++ admin/conn.php
@@ -2,7 +2,7 @@
 $host = 'localhost'; // Your database server hostname
 $username = 'root'; // Your database username
 $password = ''; // Your database password
-$database = 'elavatetaxsolution'; // Your database name
+$database = ''; // Your database name
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
